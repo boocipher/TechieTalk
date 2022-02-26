8ba9be81ac9432e740fa617023b1c491c06c0e30
boocipher <boocipher@gmail.com>
2022-02-26 11:09:28 -0600

    Adds auth.js

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,7 @@
+// Import our necessary libraries and models
+
+// Post Route and should use WithAuth to verify that user is logged IN
+    // NOTE: to test with insomnia you can delete the withAuth and test the route and add it after
+
+// Export the routes
+// and import to api index.js
---
+++ controllers/api/post-routes.js
@@ -0,0 +1,11 @@
+// Import our necessary libraries and models
+
+// Post Route and should use WithAut to verify that user is logged IN
+    // NOTE: to test with insomnia you can delete the withAuth and test the route and add it after
+
+// Put route also with withAuth
+
+// Delete Route also with withAuth
+
+// Export the routes
+// and import to api index.js
